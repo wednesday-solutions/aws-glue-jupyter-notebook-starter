f6ea88b4b46f1f088f97a575c58972dbfa920a8d
Mac <mohammed.ali.chherawalla@gmail.com>
2023-08-14 13:29:04 +0530

    feat: update teardown script

--- scripts/tear-down-glue.sh
+++ scripts/tear-down-glue.sh
@@ -3,8 +3,6 @@
 export "REGION=$(yq e '.region' config/properties.yml)"
 export "STACK_NAME=$(yq e '.stack_name' config/properties.yml)"
 export "BUCKET_NAME=$(yq e '.bucket_name' config/properties.yml)"
-
+aws s3 rm "s3://$BUCKET_NAME" --recursive
 echo "aws cloudformation delete-stack --stack-name $STACK_NAME --region $REGION"
 aws cloudformation delete-stack --stack-name $STACK_NAME --region $REGION
-aws s3 rm "s3://$BUCKET_NAME" --recursive
-aws s3api delete-bucket --bucket $BUCKET_NAME
