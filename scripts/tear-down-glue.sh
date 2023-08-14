2a6604bff005e4c2cfa10cbd075dcb99edfe5ee7
Mac <mohammed.ali.chherawalla@gmail.com>
2023-08-14 13:00:07 +0530

    feat: add support to empty and delete bucket on teardown

--- scripts/tear-down-glue.sh
+++ scripts/tear-down-glue.sh
@@ -2,6 +2,9 @@
 
 export "REGION=$(yq e '.region' config/properties.yml)"
 export "STACK_NAME=$(yq e '.stack_name' config/properties.yml)"
+export "BUCKET_NAME=$(yq e '.bucket_name' config/properties.yml)"
 
 echo "aws cloudformation delete-stack --stack-name $STACK_NAME --region $REGION"
 aws cloudformation delete-stack --stack-name $STACK_NAME --region $REGION
+aws s3 rm "s3://$BUCKET_NAME" --recursive
+aws s3api delete-bucket --bucket $BUCKET_NAME
