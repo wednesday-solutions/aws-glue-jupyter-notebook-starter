2557e4844d7167a5ceb27a17b521b6d8371673c2
Mac <mohammed.ali.chherawalla@gmail.com>
2023-08-16 16:46:09 +0530

    feat: fix update-glue-job and limit max retries to 0

--- scripts/update-glue-job.sh
+++ scripts/update-glue-job.sh
@@ -1,28 +1,72 @@
-#!/bin/sh 
+#!/bin/sh -x
+
 STACK_NAME=$1
 REGION=$2
 
-aws cloudformation create-change-set \
-  --stack-name $STACK_NAME \
-  --template-body file://assets/output.yaml \
-  --region $REGION \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --change-set-name $STACK_NAME-change-set >> /dev/null
+# Function to create a change set
+create_change_set() {
+  aws cloudformation create-change-set \
+    --stack-name $STACK_NAME \
+    --template-body file://assets/output.yaml \
+    --region $REGION \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --change-set-name $STACK_NAME-change-set >> /dev/null
+}
 
-# Describe the change set to see if there are changes
-changes=$(aws cloudformation describe-change-set \
-  --stack-name $STACK_NAME \
-  --change-set-name $STACK_NAME-change-set \
-  --query 'StatusReason')
-
-
-echo $changes
-
-# If changes are present, execute the change set
-if [ "$changes" != "\"The submitted information didn't contain changes. Submit different information to create a change set.\"" ]; then
+# Function to execute a change set
+execute_change_set() {
   aws cloudformation execute-change-set \
     --stack-name $STACK_NAME \
-    --change-set-name $STACK_NAME-change-set
-else
-  echo "No changes to apply"
-fi
+    --change-set-name $STACK_NAME-change-set \
+    --region $REGION  >> /dev/null
+}
+
+delete_change_set() {
+  aws cloudformation delete-change-set \
+    --stack-name $STACK_NAME \
+    --change-set-name $STACK_NAME-change-set \
+    --region $REGION  >> /dev/null
+}
+ 
+describe_check_and_execute () {
+  # Check if the change set already exists
+  change_set=$(aws cloudformation describe-change-set \
+    --stack-name $STACK_NAME \
+    --change-set-name $STACK_NAME-change-set \
+    --region $REGION \
+    --query 'StatusReason')
+
+  echo $change_set
+
+
+  no_new_info=$(echo $change_set | grep "Submit different information to create a change set")
+
+  mismatch=$(echo $change_set | grep "mismatch with existing attribute Template")
+
+  echo "no_new_info:"$no_new_info
+  echo "mismatch:"$mismatch
+
+  if [ -n "$no_new_info" ]; then
+    echo "No new information in change set. Deleting it."
+    delete_change_set
+    exit 0
+
+  elif [ -n "$mismatch" ]; then
+    echo "Mismatch in change set. Deleting it"
+    delete_change_set
+  elif [ -n "$change_set" ]; then
+    echo "Change set already exists. Executing it."
+    execute_change_set
+    exit 0
+  fi
+}
+
+
+
+describe_check_and_execute
+
+create_change_set
+
+sleep 10
+
+describe_check_and_execute
